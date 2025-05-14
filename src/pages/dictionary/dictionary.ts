41dddb1ab080e5931375be95599494341192fb39
fipark <fipark2002@naver.com>
2025-05-14 16:33:50 +0900

    💄 도감2차 작업업

--- src/pages/dictionary/dictionary.ts
+++ src/pages/dictionary/dictionary.ts
@@ -1,68 +1,18 @@
-interface PokemonData {
-  id: number;
-  name: string;
-  image: string | null;
-}
+const pokeball = document.querySelector('.pokeball');
+const typeModal = document.getElementById('type-modal');
+const closeTypeModal = document.getElementById('close-type-modal');
 
-interface NameEntry {
-  language: {
-    name: string;
-  };
-  name: string;
-}
+pokeball?.addEventListener('click', () => {
+  typeModal?.classList.remove('hidden');
+});
 
-interface PokemonResponse {
-  name: string;
-  sprites: {
-    front_default: string | null;
-  };
-}
+closeTypeModal?.addEventListener('click', () => {
+  typeModal?.classList.add('hidden');
+});
 
-interface PokemonSpeciesResponse {
-  names: NameEntry[];
-}
-
-const pokemonIds: number[] = Array.from({ length: 151 }, (_, i) => i + 1);
-
-const fetchPokemonList = async (): Promise<void> => {
-  const allData: PokemonData[] = await Promise.all(
-    pokemonIds.map(async (id): Promise<PokemonData> => {
-      const [pokemonRes, speciesRes]: [
-        PokemonResponse,
-        PokemonSpeciesResponse,
-      ] = await Promise.all([
-        fetch(`https://pokeapi.co/api/v2/pokemon/${id}`).then(res =>
-          res.json(),
-        ),
-        fetch(`https://pokeapi.co/api/v2/pokemon-species/${id}`).then(res =>
-          res.json(),
-        ),
-      ]);
-
-      const nameKo =
-        speciesRes.names.find(n => n.language.name === 'ko')?.name ||
-        pokemonRes.name;
-      const imageUrl = pokemonRes.sprites.front_default;
-
-      return {
-        id,
-        name: nameKo,
-        image: imageUrl,
-      };
-    }),
-  );
-
-  const container = document.getElementById('pokedex') as HTMLElement;
-  container.innerHTML = allData
-    .map(
-      p => `
-    <div class="card">
-      <img src="${p.image}" alt="${p.name}" />
-      <p>${p.name}</p>
-    </div>
-  `,
-    )
-    .join('');
-};
-
-fetchPokemonList();
+// 바깥 클릭 시 닫함
+typeModal?.addEventListener('click', e => {
+  if (e.target === typeModal) {
+    typeModal.classList.add('hidden');
+  }
+});
