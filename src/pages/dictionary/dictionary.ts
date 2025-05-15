57f7bad8d7eee3741422befbefd5e38e2a4969df
fipark <fipark2002@naver.com>
2025-05-15 15:09:59 +0900

    ✨ api테스트트

--- src/pages/dictionary/dictionary.ts
+++ src/pages/dictionary/dictionary.ts
@@ -16,3 +16,60 @@ typeModal?.addEventListener('click', e => {
     typeModal.classList.add('hidden');
   }
 });
+
+/////////////////////////////api 불러오기////////////////////////////////
+
+//기본 틀//
+const pokedex = document.getElementById('pokedex');
+
+type Pokemon = {
+  name: string;
+  imgUrl: string;
+  number: string;
+};
+//포켓몬 1마리 불러오는 함수수(pomise.all이 동시에 api를 불러오는 메소드라 하네요요)
+const fetchPokemon = async (id: number): Promise<Pokemon> => {
+  const [pokemonRes, speciesRes] = await Promise.all([
+    fetch(`https://pokeapi.co/api/v2/pokemon/${id}`).then(res => res.json()),
+    fetch(`https://pokeapi.co/api/v2/pokemon-species/${id}`).then(res =>
+      res.json(),
+    ),
+  ]);
+  type NameEntry = {
+    language: {
+      name: string;
+    };
+    name: string;
+  };
+
+  const nameKor =
+    speciesRes.names.find((n: NameEntry) => n.language.name === 'ko')?.name ||
+    pokemonRes.name;
+
+  const imgUrl = pokemonRes.sprites.front_default;
+  const number = String(id).padStart(3, '0');
+  return { name: nameKor, imgUrl, number };
+};
+//카드 안에 내용을 #pokedex에 추가
+const renderPokemonCard = (pokemon: Pokemon) => {
+  const card = document.createElement('div');
+  card.className = 'pokemon-card';
+  card.innerHTML = `
+    <img src="${pokemon.imgUrl}" alt="${pokemon.name}" />
+    <div class="poke-num">#${pokemon.number}</div>
+    <div class="poke-name">${pokemon.name}</div>
+  `;
+  pokedex?.appendChild(card);
+};
+
+//151번까지 반복
+const fetchAllPokemon = async () => {
+  if (!pokedex) return;
+  pokedex.innerHTML = '';
+  for (let i = 1; i <= 151; i++) {
+    const pokemon = await fetchPokemon(i);
+    renderPokemonCard(pokemon);
+  }
+};
+
+fetchAllPokemon();
