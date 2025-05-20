b6c0d45640c1a44503121ab98cd13266e3310af4
onewayay <onewayay@naver.com>
2025-05-20 16:30:19 +0900

    ✨ 도감페이지 공통 버튼 추가

--- src/pages/dictionary/dictionary.ts
+++ src/pages/dictionary/dictionary.ts
@@ -1,4 +1,15 @@
 import '../../common/total-time.ts'; // 누적 플레이 타임
+import soundOn from '/src/assets/common/sound-on.png'; // sound-on 이미지
+import soundOff from '/src/assets/common/sound-off.png'; // sound-off 이미지
+import { musicPlay } from '../../common/local-storage.ts'; // 현재 로컬스토리지의 음소거 상태
+import { toggleSound } from '../../common/toggle-sound.ts'; // 음악 켜기 / 끄기 기능
+import { allowMusic } from '../../common/music.ts';
+import dictionaryMusicSrc from '/src/assets/music/dictionary-music.mp3';
+
+// town-music 오디오 객체 생성 및 음악 재생
+const dictionaryMusic = new Audio(dictionaryMusicSrc);
+dictionaryMusic.volume = 0.3;
+allowMusic(dictionaryMusic, true);
 
 const pokeball = document.querySelector('.pokeball');
 const typeModal = document.getElementById('type-modal');
@@ -316,3 +327,68 @@ loadOwnedPokemon();
 // fetchPokemon(id) 는 각 포켓몬 데이터를 api에서 가져오는 함수
 // fetchAllPokemon()는 1세대 포켓몬 151마리 전부 가져오는 함수
 // renderPokemonList() / renderPokemonCard()는 도감에 포켓몬들을 표시하는 함수 revealed가 true면 포켓몬 이미지 false면 ???로 표시
+
+// ST : 뒤로가기, 음소거 버튼 ------------------
+const backBtn = document.querySelector('.back-btn') as HTMLElement;
+const toggleSoundBtn = document.querySelector('.toggle-sound') as HTMLElement;
+const toggleSoundText = document.querySelector(
+  '.toggle-sound > span',
+) as HTMLElement;
+
+// 버튼 및 span의 텍스트 초기화
+if (musicPlay() === 'true') {
+  toggleSoundBtn.style.backgroundImage = `url(${soundOn})`;
+  toggleSoundText.innerHTML = '전체 소리 끄기 버튼';
+} else {
+  toggleSoundBtn.style.backgroundImage = `url(${soundOff})`;
+  toggleSoundText.innerHTML = '전체 소리 켜기 버튼';
+}
+
+// 뒤로가기
+backBtn.addEventListener('click', () => {
+  window.history.back();
+});
+
+// 음소거/재생
+toggleSoundBtn.addEventListener('click', () => {
+  const soundState: string | null = musicPlay();
+  toggleSound(dictionaryMusic);
+  if (soundState === 'true') {
+    toggleSoundBtn.style.backgroundImage = `url(${soundOff})`;
+    toggleSoundText.innerHTML = '전체 소리 켜기 버튼';
+  } else {
+    toggleSoundBtn.style.backgroundImage = `url(${soundOn})`;
+    toggleSoundText.innerHTML = '전체 소리 끄기 버튼';
+  }
+});
+
+// 640기준으로 뒤로가기, 음소거/재생 마우스 이벤트 등록/제거
+function topBtnHover() {
+  const winW: number = window.innerWidth;
+  if (winW > 640) {
+    backBtn.style.opacity = '0.7';
+    toggleSoundBtn.style.opacity = '0.7';
+    backBtn.addEventListener('mouseenter', () => {
+      backBtn.style.opacity = '1';
+    });
+    backBtn.addEventListener('mouseleave', () => {
+      backBtn.style.opacity = '0.7';
+    });
+    toggleSoundBtn.addEventListener('mouseenter', () => {
+      toggleSoundBtn.style.opacity = '1';
+    });
+    toggleSoundBtn.addEventListener('mouseleave', () => {
+      toggleSoundBtn.style.opacity = '0.7';
+    });
+  } else {
+    backBtn.style.opacity = '1';
+    toggleSoundBtn.style.opacity = '1';
+  }
+}
+
+// 리사이즈 이벤트로 브라우저 사이즈 달라질 때마다 이벤트동작
+window.addEventListener('resize', topBtnHover);
+// 초기 동작
+topBtnHover();
+
+// ED : 뒤로가기, 음소거 버튼 ------------------
